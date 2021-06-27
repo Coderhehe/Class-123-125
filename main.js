2333f7338eadbe2b695be137be79f43a3a46bffb
Coderhehe <77240747+Coderhehe@users.noreply.github.com>
2021-06-27 10:58:05 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+nosex=0;
+nosey=0;
+difference=0;
+leftwx=0;
+rightwx=0;
 function setup() {
     video= createCapture(VIDEO);
     video.size(550,500);
@@ -13,11 +18,21 @@ function modelLoaded(){
 
 function draw() {
     background(51);
+document.getElementById("spansquare").innerHTML="Width And Height Of Square Is"+difference;
+fill("#fcf403");
+stroke("#fcf403");
+square(nosex,nosey,difference);
 }
 
 
 function gotResult(result) {
    if(result.length > 0){
 console.log(result);
+nosex=result[0].pose.nose.x;
+nosey=result[0].pose.nose.x;
+console.log("Nose X = "+nosex+"Nose Y = "+nosey);
+leftwx=result[0].pose.leftWrist.x;
+rightwx=result[0].pose.rightWrist.x;
+difference=floor(leftwx-rightwx)
    } 
 }
